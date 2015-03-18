76697204e7d21cb45378d31350f338261873a9ee
Vladimir Kryachko <v.kryachko@gmail.com>
2015-03-18 13:24:35 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ import os
 
 setup(
     name = "django-piston",
-    version = "0.2.6.etv",
+    version = "0.2.7.etv",
     url = 'http://bitbucket.org/jespern/django-piston/wiki/Home',
 	download_url = 'http://bitbucket.org/jespern/django-piston/downloads/',
     license = 'BSD',
